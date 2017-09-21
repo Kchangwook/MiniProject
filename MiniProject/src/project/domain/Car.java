5c3ba7b29599cb83597e34035f3af36a9e1627d5
Kosta <Kosta@Kosta-HP>
2017-09-21 18:12:17 +0900

    DTO완성

--- MiniProject/src/project/domain/Car.java
+++ MiniProject/src/project/domain/Car.java
@@ -1,5 +1,84 @@
 package project.domain;
 
+/** DB로부터 Car Table의 정보를 담을 클래스 */
 public class Car {
 
-}
+	/* 변수 */
+	private int carNum;
+	private int carPrice;
+	private String carSize;
+	private int carMaxDistance;
+	private String carEfficiency;
+	private String carImgSrc;
+	
+	/* 생성자 */ 
+	public Car() {}
+	public Car(int carNum, int carPrice, String carSize, int carMaxDistance, String carEfficiency, String carImgSrc) {
+		super();
+		this.carNum = carNum;
+		this.carPrice = carPrice;
+		this.carSize = carSize;
+		this.carMaxDistance = carMaxDistance;
+		this.carEfficiency = carEfficiency;
+		this.carImgSrc = carImgSrc;
+	}
+	
+	/* 접근자, 수정자 */
+	public int getCarNum() {
+		return carNum;
+	}
+	public void setCarNum(int carNum) {
+		this.carNum = carNum;
+	}
+	public int getCarPrice() {
+		return carPrice;
+	}
+	public void setCarPrice(int carPrice) {
+		this.carPrice = carPrice;
+	}
+	public String getCarSize() {
+		return carSize;
+	}
+	public void setCarSize(String carSize) {
+		this.carSize = carSize;
+	}
+	public int getCarMaxDistance() {
+		return carMaxDistance;
+	}
+	public void setCarMaxDistance(int carMaxDistance) {
+		this.carMaxDistance = carMaxDistance;
+	}
+	public String getCarEfficiency() {
+		return carEfficiency;
+	}
+	public void setCarEfficiency(String carEfficiency) {
+		this.carEfficiency = carEfficiency;
+	}
+	public String getCarImgSrc() {
+		return carImgSrc;
+	}
+	public void setCarImgSrc(String carImgSrc) {
+		this.carImgSrc = carImgSrc;
+	}
+	
+	/* 함수 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Car [carNum=");
+		builder.append(carNum);
+		builder.append(", carPrice=");
+		builder.append(carPrice);
+		builder.append(", carSize=");
+		builder.append(carSize);
+		builder.append(", carMaxDistance=");
+		builder.append(carMaxDistance);
+		builder.append(", carEfficiency=");
+		builder.append(carEfficiency);
+		builder.append(", carImgSrc=");
+		builder.append(carImgSrc);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}//end of Car
--- MiniProject/src/project/domain/Station.java
+++ MiniProject/src/project/domain/Station.java
@@ -1,5 +1,85 @@
 package project.domain;
 
+/** DB의 Station Table의 정보를 저장하기 위한 클래스 */
 public class Station {
 
-}
+	/* 변수 */
+	private int stationNum;
+	private String statingAddr;
+	private String stationCsnm;
+	private String stationCpnm;
+	private int stationLat;
+	private int stationLongi;
+	
+	/* 생성자 */
+	public Station() {}
+	public Station(int stationNum, String statingAddr, String stationCsnm, String stationCpnm, int stationLat,
+			int stationLongi) {
+		super();
+		this.stationNum = stationNum;
+		this.statingAddr = statingAddr;
+		this.stationCsnm = stationCsnm;
+		this.stationCpnm = stationCpnm;
+		this.stationLat = stationLat;
+		this.stationLongi = stationLongi;
+	}
+	
+	/* 설정자 수정자 */
+	public int getStationNum() {
+		return stationNum;
+	}
+	public void setStationNum(int stationNum) {
+		this.stationNum = stationNum;
+	}
+	public String getStatingAddr() {
+		return statingAddr;
+	}
+	public void setStatingAddr(String statingAddr) {
+		this.statingAddr = statingAddr;
+	}
+	public String getStationCsnm() {
+		return stationCsnm;
+	}
+	public void setStationCsnm(String stationCsnm) {
+		this.stationCsnm = stationCsnm;
+	}
+	public String getStationCpnm() {
+		return stationCpnm;
+	}
+	public void setStationCpnm(String stationCpnm) {
+		this.stationCpnm = stationCpnm;
+	}
+	public int getStationLat() {
+		return stationLat;
+	}
+	public void setStationLat(int stationLat) {
+		this.stationLat = stationLat;
+	}
+	public int getStationLongi() {
+		return stationLongi;
+	}
+	public void setStationLongi(int stationLongi) {
+		this.stationLongi = stationLongi;
+	}
+	
+	/* 함수 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Station [stationNum=");
+		builder.append(stationNum);
+		builder.append(", statingAddr=");
+		builder.append(statingAddr);
+		builder.append(", stationCsnm=");
+		builder.append(stationCsnm);
+		builder.append(", stationCpnm=");
+		builder.append(stationCpnm);
+		builder.append(", stationLat=");
+		builder.append(stationLat);
+		builder.append(", stationLongi=");
+		builder.append(stationLongi);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}//end of Station
