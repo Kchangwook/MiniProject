600dadd5882789e36c13fa66dc610f3f40b262d7
Joung TaeJune <jtj0525@naver.com>
2017-09-21 18:10:52 +0900

    2017-09-21 18:10 by june

--- MiniProject/projects.sql
+++ MiniProject/projects.sql
@@ -1,4 +1,11 @@
+--system[Admin계정에서 evcar계정 생성&권한부여
+create user evcar identified by 1234;
+grant connect,resource,dba to evcar;
 
+--테이블 확인
+select * from tab;
+
+--지우는 순서
 drop sequence free_num;
 drop sequence faq_num;
 drop table free;
@@ -11,54 +18,51 @@ create sequence free_num start with 1 increment by 1;
 create sequence faq_num start with 1 increment by 1;
 
 create table Car(
-car_num	number,
-car_price	number,
-car_size	varchar(20),
-car_max_distance	number,
-car_efficiency	varchar(20),
-car_img_src	varchar(200),
+car_num	            NUMBER,
+car_price	        NUMBER          not null,
+car_size	        VARCHAR2(20)    not null,
+car_max_distance	NUMBER          not null,
+car_efficiency	    VARCHAR2(20)    not null,
+car_img_src	        VARCHAR2(200)   not null,
 constraint car_num_pk primary key(car_num)
 );
 
 create table Station(
-station_num	    number,
-station_addr	VARCHAR(255) not null,
-station_csnm	VARCHAR(100) not null,
-station_cpnm	VARCHAR(100) not null,
-station_lat	    number      not null,
-station_longi	number      not null,
+station_num	        NUMBER,
+station_addr	    VARCHAR2(255)   not null,
+station_csnm	    VARCHAR2(100)   not null,
+station_cpnm	    VARCHAR2(100)   not null,
+station_lat	        NUMBER          not null,
+station_longi	    NUMBER          not null,
 constraint station_num_pk primary key(station_num)
 );
 
 create table Member(
-member_id	VARCHAR(50),
-member_pwd	VARCHAR(50),
-member_name	VARCHAR(50),
-member_mail	VARCHAR(50),
-member_addr	VARCHAR(255),
-member_phone	VARCHAR(15),
-member_level	number,
+member_id	        VARCHAR2(50),
+member_pwd	        VARCHAR2(50)    not null,
+member_name	        VARCHAR2(50)    not null,
+member_mail	        VARCHAR2(50)    not null,
+member_addr	        VARCHAR2(255)   not null,
+member_phone	    VARCHAR2(15)    not null,
+member_level	    NUMBER          not null,
 constraint member_id_pk primary key(member_id)
 );
 
 create table Free(
-Free_num	number,
-Free_title	VARCHAR(50),
-Free_content	VARCHAR(1024),
-member_id	VARCHAR(50),
-Free_notice	number,
+Free_num	        NUMBER,
+Free_title	        VARCHAR2(50)    not null,
+Free_content	    VARCHAR2(3000)  not null,
+member_id	        VARCHAR2(50)    not null,
+Free_notice	        NUMBER          not null,
 constraint free_num_pk primary key(free_num),
 constraint free_member_id_fk foreign key(member_id) references member(member_id)
 );
 
 create table FAQ(
-faq_num	number,
-faq_title	VARCHAR(50),
-faq_content	VARCHAR(1024),
+faq_num	            NUMBER,
+faq_title	        VARCHAR2(50)    not null,
+faq_content	        VARCHAR2(3000)  not null,
 constraint faq_num_pk primary key(faq_num)
 );
-
-
-select * from station;
 
 commit;
